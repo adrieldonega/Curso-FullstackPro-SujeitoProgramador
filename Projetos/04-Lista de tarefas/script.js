0294d0bb425343f66f472f4c5c836458067576db
Adriel Donega <adrieldonega@live.com>
2025-05-27 14:51:36 -0300

    Upload To-Do List

--- Projetos/04-Lista de tarefas/script.js
+++ Projetos/04-Lista de tarefas/script.js
@@ -1,13 +1,20 @@
 
 //querySelector -> Seleciona o primeiro elemento em um documento HTML que corresponda a um seletor CSS especificado.
 
+// .splice() -> remove o item do Array
+// .indexOf() -> descobre em qual posição o item se encontra.
+
+//_________________________________________________________________PRIMEIRO PASSO - DECLARAÇÃO DAS VARIAVEIS E DECLARAÇÃO DA LISTA (ARRAY)
 let listElement = document.querySelector("#app ul");
 let inputElement = document.querySelector("#app input");
 let buttonElement = document.querySelector("#app button");
 
 //Armazenando a lista de tarefas dentro de um array, a lista será preenchida automaticamente dentro do array.
-let tarefas = [];
+let tarefas = JSON.parse(localStorage.getItem("@listaTarefas")) || [];
+//_________________________________________________________________FIM PRIMEIRO PASSO
 
+
+//_________________________________________________________________TERCEIRO PASSO - FAZENDO APARECER A TAREFA NO DOCUMENTO HTML
 function renderTarefas() {
     listElement.innerHTML = "";  //Acessa a UL do documento HTML e zera a array caso tenha algo dentro dela.
 
@@ -15,21 +22,39 @@ function renderTarefas() {
 
     // .map() -> percorre todos os itens dentro da array, e dentro do map posso ter acesso ao elemento que está passando.  
     tarefas.map((todo) => {
+
         // .createElement -> Cria um elemento no documento. No caso um (li)
         let liElement = document.createElement("li");
 
         // .createTextNode -> Cria um texto 
         let tarefaText = document.createTextNode(todo);
 
+        // Criando uma ancora para o botao de excluir
+        let linkElement = document.createElement("a");
+
+        // .setAtribute -> Adiciona um atributo ao elemento
+        linkElement.setAttribute("href", "#");
+
+        let linkText = document.createTextNode("Excluir");
+        linkElement.appendChild(linkText);
+
+        let posicao = tarefas.indexOf(todo);
+
+        linkElement.setAttribute("onclick", `deletarTarefa(${posicao})`);
+
 
         //Adicionando na ul o (todo)
         liElement.appendChild(tarefaText);
+        liElement.appendChild(linkElement);
         listElement.appendChild(liElement);
     })
 
 }
+//___________________________________________________________________FIM TERCEIRO PASSO 
 
+renderTarefas();
 
+//___________________________________________________________________SEGUNDO PASSO - INICIALIZANDO A FUNÇÃO DE ADICIONAR TAREFAS E ADICIONANDO UMA FUNÇÃO NO BOTAO
 function adicionarTarefas() {
 
     //se o INPUT estiver vazio, emitir um alerta.
@@ -45,9 +70,24 @@ function adicionarTarefas() {
         inputElement.value = ''; //limpa o input
 
         renderTarefas();
+        salvarDados();
 
 
     }
 }
 
 buttonElement.onclick = adicionarTarefas;
+
+//___________________________________________________________________FIM SEGUNDO PASSO
+
+function deletarTarefa(posicao) {
+    tarefas.splice(posicao, 1);
+    renderTarefas();
+    salvarDados();
+}
+
+
+// CRIANDO FUNÇÃO PARA SALVAR NO LOCAL STORAGE
+function salvarDados() {
+    localStorage.setItem("@listaTarefas", JSON.stringify(tarefas))
+}
